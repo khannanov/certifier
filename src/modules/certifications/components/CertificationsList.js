b50d7e0269df79d74a5e03e96c47cc2ca2c72a02
khannanov <khannanov.aidar@gmail.com>
2017-03-29 00:35:33 +0300

    use cards

--- src/modules/certifications/components/CertificationsList.js
+++ src/modules/certifications/components/CertificationsList.js
@@ -1,27 +1,37 @@
 import React, { PropTypes } from 'react'
 import IconEditorMode from 'material-ui/svg-icons/editor/mode-edit'
+import IconTimer from 'material-ui/svg-icons/image/timer'
 import IconButton from 'material-ui/IconButton'
-import Divider from 'material-ui/Divider'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 
 const CertificationsList = (props) => {
-
   const certifications = props.certifications.map(
     certification =>
-      <section key={certification.id}>
-        <div>
-          <h2>{certification.name}</h2>
-          <p>{certification.description}</p>
-          <IconButton tooltip="edit"
-                      touch={true}
-                      tooltipPosition="top-center"
-                      href={`/edit/${certification.id}`}
-          >
-            <IconEditorMode/>
-          </IconButton>
-        </div>
-        <Divider />
-      </section>
+      <Card key={certification.id} style={{marginBottom: 20}}>
+        <CardHeader
+          title={certification.name}
+          titleStyle={{fontSize: 24}}
+          subtitle={certification.description}
+        />
+        <CardActions>
+          <RaisedButton label="Start"
+                        primary={true}
+                        labelPosition="before"
+                        href={`/edit/${certification.id}`}
+                        icon={<IconTimer/>}
+          />
+          <RaisedButton label="Edit"
+                        primary={true}
+                        href={`/edit/${certification.id}`}
+                        labelPosition="before"
+                        icon={<IconEditorMode/>}
+          />
+        </CardActions>
+      </Card>
   )
+
 
   return <div>{certifications}</div>
 }
